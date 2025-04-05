5eb5e769297bdb98c618f6d797eccb0819cdf4f4
Hanif Istifarulah <hanifrifaii@gmail.com>
2025-04-05 22:39:14 +0700

    Update config for auto launch app, adding array example for multiple game config

--- config.py
+++ config.py
@@ -1,8 +1,20 @@
 EGPU_NAME = "AMD Radeon RX 6800 XT"
+APP_AUTO_LAUNCH = True
+APP_PATH = "C:\Program Files\AMD\CNext\CNext\RadeonSoftware.exe"
 GAME_PROFILES = [
     {
         "d_config_path": r"C:\egpu-listener-auto-game-config\dgpu\forza5\UserConfigSelections",
         "i_config_path": r"C:\egpu-listener-auto-game-config\igpu\forza5\UserConfigSelections",
         "destination": r"C:\Users\hanif\AppData\Local\ForzaHorizon5\User_SteamLocalStorageDirectory\ConnectedStorage\ForzaUserConfigSelections",
-    }
+    },
+    {
+        "d_config_path": r"C:\egpu-listener-auto-game-config\dgpu\division2\graphic settings.cfg",
+        "i_config_path": r"C:\egpu-listener-auto-game-config\igpu\division2\graphic settings.cfg",
+        "destination": r"C:\Users\hanif\OneDrive\Documents\My Games\Tom Clancy's The Division 2",
+    },
+    {
+        "d_config_path": r"C:\egpu-listener-auto-game-config\dgpu\division2\state.cfg",
+        "i_config_path": r"C:\egpu-listener-auto-game-config\igpu\division2\state.cfg",
+        "destination": r"C:\Users\hanif\OneDrive\Documents\My Games\Tom Clancy's The Division 2",
+    },
 ]
